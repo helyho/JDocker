001b7d3cf25380dacf745634e3a08cf6c3898aba
helyho <helyho@gmail.com>
2017-01-13 20:01:54 +0800

    add: Service 和 Container 创建对 cpu 和 memory 进行封装,cpu 的单位为: cpu 核心数,内存的单位为: mb

---
+++ src/main/org/voovan/docker/command/CmdDockerFlyConfig.java
@@ -0,0 +1,34 @@
+package org.voovan.docker.command;
+
+import org.voovan.docker.DockerGlobal;
+
+/**
+ * 类文字命名
+ *
+ * @author helyho
+ *         <p>
+ *         JDocker Framework.
+ *         WebSite: https://github.com/helyho/JDocker
+ *         Licence: Apache v2 License
+ */
+public class CmdDockerFlyConfig {
+
+    public static void config(String host, int port, String charSet, int timeout, boolean isDebug){
+        DockerGlobal.DOCKER_REST_HOST = host;
+        DockerGlobal.DOCKER_REST_PORT = port;
+        DockerGlobal.DOCKER_REST_CHARSET = charSet;
+        DockerGlobal.DOCKER_REST_TIMEOUT = timeout;
+        DockerGlobal.DEBUG = isDebug;
+    }
+
+    public static void config(String host, int port ){
+        DockerGlobal.DOCKER_REST_HOST = host;
+        DockerGlobal.DOCKER_REST_PORT = port;
+    }
+
+    public static void config(String host, int port, boolean isDebug){
+        DockerGlobal.DOCKER_REST_HOST = host;
+        DockerGlobal.DOCKER_REST_PORT = port;
+        DockerGlobal.DEBUG = isDebug;
+    }
+}
